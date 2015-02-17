d648f00b2ea4dfe2d0537a13b82056044a3f3678
lafigueroa <lafigueroa@brainycode.com>
2015-02-17 14:10:41 -0500

    Added Unit Testing Infrastructure
    Added ChessUnit Unit Testing Infrastructure, allowing us to have proper
    Unit Testing in a Unity Scene. Provided example Position class with
    related Unit Test Cases to demonstrate infrastructure.

---
+++ chess451/Assets/DigitalForest/Scripts/Piece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// Template Pattern for Chess Pieces. Used for generic containers such as board positions and for testing.
+/// Author: qtr23
+/// </summary>
+///
+/// 
+public abstract class Piece : MonoBehaviour
+{
+    Position pos;
+
+    
+}
