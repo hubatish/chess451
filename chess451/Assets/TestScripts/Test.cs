865c41bcc3fe2dcc0cff7de13424b13a9d8546b6
hubatish <Zach@Aesop.(none)>
2015-02-19 14:19:52 -0500

    Moved things around and added git ignore

---
+++ chess451/Assets/TestScripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CTest  {
+
+    public  string testname;
+
+    /// <summary>
+    /// Runs the full set of testcases for this Test Suite. First line should always set the testname of the suite
+    /// </summary>
+    /// <param name="message">Message to be passed back to Suite Runner. Should include details of all failed test casses, or the message "Passed" if no test case failed</param>
+    /// <returns>False if atleast one testcase fails, and true otherwise</returns>
+    public abstract bool run(out string message);
+}
---
+++ chess451/Assets/TestScripts/TestRunner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRunner : MonoBehaviour {
+
+    ArrayList tests;
+
+    void Awake()
+    {
+        tests = new ArrayList();
+
+        // Add Tests here
+        tests.Add(new PositionTest());
+    }
+	// Use this for initialization
+	void Start () {
+        bool passed = true;
+	foreach (CTest test in tests)
+      {
+          string message;
+          if (!test.run(out message))
+          {
+              Debug.Log(test.testname + " failed with message " + message);
+              passed = false;
+          }
+        }
+        if (passed)
+        {
+            Debug.Log("All Test Cases pass!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
