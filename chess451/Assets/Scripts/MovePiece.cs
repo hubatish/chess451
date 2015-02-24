2a5813cdbb81611e94a3ecaa7142aa0aa9068296
Xavi <xds23@drexel.edu>
2015-02-24 18:09:01 -0500

    Code +Edited movePiece to work properly (you can now select a piece and move it anywhere on the board). +movePIece is now attatched to the board object instead of the pieces.
    Misc
    +Made squares larger for better collision detection
    +Squares all added to project and labelled.
    +Pieces now fall under "BlackPiece" (White Piece to be added later)

--- chess451/Assets/Scripts/MovePiece.cs
+++ chess451/Assets/Scripts/MovePiece.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MovePiece : MonoBehaviour {
 
-	public GameObject square;
+	public GameObject sPiece; //Selected piece
 	private Vector3 newPosition;
 
 	// Use this for initialization
@@ -17,12 +17,23 @@ public class MovePiece : MonoBehaviour {
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
 
+
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
-			if (Physics.Raycast (ray, out hit, 100)) {
+
+			if(sPiece == null) //select piece
+			{
+				if (Physics.Raycast (ray, out hit, 100)) 
+				{
+					sPiece = hit.transform.gameObject;
+				}
+			}
+			else if (Physics.Raycast (ray, out hit, 100)) {
 				newPosition.x = hit.transform.position.x;
+				newPosition.y = sPiece.transform.position.y; //so that we dont fly off high
 				newPosition.z = hit.transform.position.z;
-				transform.position = newPosition;
+				sPiece.transform.position = newPosition;
 				Debug.Log (hit.transform.gameObject.name);
+				sPiece = null;
 			}
 		}
 		/*
