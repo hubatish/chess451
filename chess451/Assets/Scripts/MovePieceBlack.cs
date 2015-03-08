eda062c178b7f8d1965a80a22747daa2843d8503
XaviSmith <xds23@drexel.edu>
2015-03-08 17:28:20 -0400

    +MovePieceWhite and MovePieceBlack now both know the coordinates they're headed to +added setPos(int x, int y) function to Piece

--- chess451/Assets/Scripts/MovePieceBlack.cs
+++ chess451/Assets/Scripts/MovePieceBlack.cs
@@ -6,6 +6,45 @@ public class MovePieceBlack : MonoBehaviour {
 	public GameObject sPiece; //Selected piece
 	private Vector3 newPosition; //Where we move the piece
 
+	int convertRow(char row)
+	{
+		switch (row) 
+		{
+		case 'A':
+			return 0;
+			break;
+			
+		case 'B':
+			return 1;
+			break;
+			
+		case 'C':
+			return 2;
+			break;
+			
+		case 'D':
+			return 3;
+			break;
+			
+		case 'E':
+			return 4;
+			break;
+			
+		case 'F':
+			return 5;
+			break;
+			
+		case 'G':
+			return 6;
+			break;
+			
+		case 'H':
+			return 7;
+			break;
+		default : return -1;
+			break;
+		}
+	}
 	// Use this for initialization
 	void Start () {
 		newPosition = transform.position;
@@ -32,6 +71,15 @@ public class MovePieceBlack : MonoBehaviour {
 			else if (Physics.Raycast (ray, out hit, 100)) 
 			{
 				newPosition.x = hit.transform.position.x;
+
+				char[] posChar = hit.transform.parent.name.ToCharArray();
+				
+				int row = convertRow (posChar[0]);
+				int column = (int)char.GetNumericValue(posChar[1]) - 1;
+				
+				//Debug.Log ("Row: " + posChar[0] + " Column: " + posChar[1]);
+				//Debug.Log ("Coordinates: " + row + "," + column);
+
 				newPosition.y = sPiece.transform.position.y; //keep height of pieces constant
 				newPosition.z = hit.transform.position.z;
 				NetworkPlayer.Instance.MovePiece(sPiece.transform.position,newPosition);
--- chess451/Assets/Scripts/MovePieceWhite.cs
+++ chess451/Assets/Scripts/MovePieceWhite.cs
@@ -4,12 +4,53 @@ using System.Collections;
 public class MovePieceWhite : MonoBehaviour {
 	public GameObject sPiece; //Selected piece
 	private Vector3 newPosition; //Where we move the piece
-	
+
+
 	// Use this for initialization
 	void Start () {
 		newPosition = transform.position;
 	}
-	
+
+	int convertRow(char row)
+	{
+		switch (row) 
+		{
+			case 'A':
+				return 0;
+				break;
+
+			case 'B':
+				return 1;
+				break;
+
+			case 'C':
+				return 2;
+				break;
+				
+			case 'D':
+				return 3;
+				break;
+			
+			case 'E':
+				return 4;
+				break;
+				
+			case 'F':
+				return 5;
+				break;
+				
+			case 'G':
+				return 6;
+				break;
+				
+			case 'H':
+				return 7;
+				break;
+		default : return -1;
+			break;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
@@ -31,8 +72,16 @@ public class MovePieceWhite : MonoBehaviour {
 			else if (Physics.Raycast (ray, out hit, 100)) 
 			{
 				newPosition.x = hit.transform.position.x;
+				char[] posChar = hit.transform.parent.name.ToCharArray();
+
+				int row = convertRow (posChar[0]);
+				int column = (int)char.GetNumericValue(posChar[1]) - 1;
+
+				//Debug.Log ("Row: " + posChar[0] + " Column: " + posChar[1]);
+				//Debug.Log ("Coordinates: " + row + "," + column);
 				newPosition.y = sPiece.transform.position.y; //keep height of pieces constant
 				newPosition.z = hit.transform.position.z;
+
 				//Call Networking with this stuff
 				//TODO: Networking and checking should probably use positions in grid coordinates rather than Unity coordinates (like A2 or [0,1] rather than things with z's and floats
 				NetworkPlayer.Instance.MovePiece(sPiece.transform.position,newPosition);
